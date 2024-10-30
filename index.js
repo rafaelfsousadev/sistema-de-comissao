e6b5cb9a3f5939d79f2600e9567cb6526b969986
Rafael Fernandes De Sousa <130863570+rafaelfsousadev@users.noreply.github.com>
2024-10-30 16:03:18 -0300

    BR

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function adicionarNumero() {
    let valor01 = document.getElementById('valor1').value
    const p = document.createElement('p')
    p.id = 'p'+contador01
-   p.textContent = `R$ ${valor01}`
+   p.textContent = valor01
    document.getElementById('campo01').appendChild(p)
    document.getElementById('valor1').value = ''
 
@@ -16,8 +16,41 @@ function adicionarNumero2() {
     let valor02 = document.getElementById('valor2').value
     const p = document.createElement('p')
     p.id = 'p'+contador01
-    p.textContent = `R$ ${valor02}`
+    p.textContent = valor02
     document.getElementById('campo02').appendChild(p)
     document.getElementById('valor2').value = ''
  
+
  }
+
+ let rafael = 0
+ let juliano = 0
+
+function somarTotal1(){
+   const campo01 = document.querySelectorAll("#campo01 p")
+
+   const arrayNumeros = Array.from(campo01, p => Number(p.innerText))
+
+   rafael = arrayNumeros.reduce(function(accumulador,valor){
+      return accumulador + valor
+
+   })
+   document.getElementById('resultado1').innerText = `Total R$${rafael}`
+
+}
+
+function somarTotal2(){
+   const campo01 = document.querySelectorAll("#campo02 p")
+
+   const arrayNumeros = Array.from(campo01, p => Number(p.innerText))
+
+   juliano = arrayNumeros.reduce(function(accumulador,valor){
+      return accumulador + valor
+
+   })
+   document.getElementById('resultado2').innerText = `Total R$${juliano}`
+
+}
+
+
+
